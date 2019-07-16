9ce7c41fd52b4becef3fc3e4a664f9edcdaaafc9
Nicholas Padgett <nickpadge@gmail.com>
2019-07-16 16:15:05 -0400

    Added logic to log output when required. Should help make debugging easier.

--- merge-rpd.py
+++ merge-rpd.py
@@ -28,6 +28,7 @@ Examples of the above files are included in this repository.
 
 """
 
+import logging
 import time
 import os 
 import sys
@@ -40,45 +41,57 @@ import csv
 # Input Parameters
 rpd_password = "Password01"
 admin_tool_exe = "C:\\Oracle\\OBIEEClient\\bi\\bitools\\bin\\admintool.cmd"
+logs = true
+# End Input Parameters
+
+# write to the log
+def log(level, message):
+	if logs:
+		logging.log(level, message)
 
 
 # save a file with the body of contents to a file named filename
 def write_file(filename, contents):
 	try:
+		log(logging.DEBUG, "Writing file: "+str(filename))
 		f = open(filename, "w")
 		f.write(contents)
 		f.close()
 	except Exception as e:
+		log(logging.ERROR, "Unable to write to file: "+str(filename)+". Cause: "+str(e))
 		raise Exception("Error writing to file: "+str(e))
 
 			
 # delete a file from the file system			
 def delete_file(f):
 	try:
+		log(logging.DEBUG,"Deleting file: "+str(f))
 		if type(f) is not str:
 			f.close()
 			f = f.name
 		if os.path.exists(f):
 			os.remove(f)
 	except Exception as e:
-		#raise Exception("Error deleting file: "+str(e))
-		pass
+		log(logging.ERROR,"Unable to delete file: "+str(f)+". Cause: "+str(e))
 
 
 # copy a file from the orig location to the dest location (optionally delete original)
 def copy_file(orig, dest, delete=False):
 	try:
+		log(logging.DEBUG, "Copying file '"+str(orig)+"' to '"+str(dest)+"'.")
 		for f in glob(orig):
 			copyfile(f, dest)
 			if delete:
 				delete_file(f)
 	except Exception as e:
+		log(logging.ERROR, "Unable to copy file: "+str(orig)+". Cause: "+str(e))
 		raise Exception("Error copying file: "+str(e))
 
 			
 # perform the three way merge for the RPD		
 def three_way_merge(original, a_current, b_modified, output_name, password, command_file_name):
 	try:
+		log(logging.DEBUG, "Starting 3 way merge.")
 		command_body = "OpenOffline " + a_current + " " + password
 		command_body += "\nMerge "+original+" "+b_modified+" decisions.csv "+password+" "+password+" "+output_name
 		command_body += "\nSaveAs "+output_name
@@ -91,12 +104,14 @@ def three_way_merge(original, a_current, b_modified, output_name, password, comm
 		# execute
 		execute_rpd_commands(command_file_name)
 	except Exception as e:
+		log(logging.ERROR, "3 way merge failed. Cause: "+str(e))
 		raise Exception("Merge RPD failed: "+str(e))
 
 
 # perform comparison of RPD files
 def compare_rpd(current_file, other_file, output_file, password, command_file_name):
 	try:
+		log(logging.DEBUG, "Starting RPD comparison.")
 		command_body = "Hide"
 		command_body += "\nOpenOffline "+current_file + " " + password
 		command_body += "\nCompare "+other_file+ " " + password + " " + output_file
@@ -109,31 +124,41 @@ def compare_rpd(current_file, other_file, output_file, password, command_file_na
 		# execute
 		execute_rpd_commands(command_file_name)
 	except Exception as e:
+		log(logging.ERROR, "Compare RPD failed. Cause: "+str(e))
 		raise Exception("Compare RPD failed: "+str(e))
 
 
 # execute an rpd command
 def execute_rpd_commands(command_file_name):
+	log(logging.DEBUG, "Executing RPD commands in file: "+str(command_file_name))
 	command_string = [admin_tool_exe, "/Command", command_file_name]
 	p = Popen(command_string, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = p.communicate()
 	errcode = p.returncode
 	if errcode != 0:
+		log(logging.ERROR, "Failed to execute command. Unknown error code: "+str(errcode))
 		raise Exception("Command failed: "+str(errcode))
 
 
 # main method	
 if __name__ == "__main__":
 	try:
+		if logs:
+			logging.basicConfig(filename="merge.log",level=logging.DEBUG)
+
 		if len(sys.argv) <= 1:
+			log(logging.ERROR, "No action argument provided.")
 			raise Exception("Not enough arguments.")
 		action = sys.argv[1]
 		
 		# merge actions
 		if action == "merge":
 			# validate input
-			if len(sys.argv) <= 5:
+			if len(sys.argv) <= 4:
+				log(logging.ERROR, "Not enough arguments for merge: "+str(sys.argv))
 				raise Exception("Not enough arguments for merge.")
+			log(logging.DEBUG,"Starting merge workflow.")
+			
 			original_rpd_path = sys.argv[2]
 			current_rpd_path = sys.argv[3]
 			modified_rpd_path = sys.argv[4]
@@ -145,9 +170,12 @@ if __name__ == "__main__":
 			decisions_temp_file = repository_path+os.path.sep+"decisions.csv"
 			
 			# files must have the .rpd extension, so we add it
-			copy_file(original_rpd_path, original_rpd_path+rpd_extension, True)
-			copy_file(current_rpd_path, current_rpd_path+rpd_extension, True)
-			copy_file(modified_rpd_path, modified_rpd_path+rpd_extension, True)
+			try:
+				copy_file(original_rpd_path, original_rpd_path+rpd_extension, True)
+				copy_file(current_rpd_path, current_rpd_path+rpd_extension, True)
+				copy_file(modified_rpd_path, modified_rpd_path+rpd_extension, True)
+			except Exception as e:
+				throw new Exception("Error when preparing RPD files: "+str(e))
 	
 			# decisions file must not be empty
 			write_file(decisions_temp_file, "Decision\n")
@@ -156,16 +184,21 @@ if __name__ == "__main__":
 			three_way_merge(original_rpd_path+rpd_extension, current_rpd_path+rpd_extension, modified_rpd_path+rpd_extension, current_rpd_path, rpd_password, command_file_name)
 			
 			# delete all the temp files we created
-			delete_file(current_rpd_path+rpd_extension)
-			delete_file(modified_rpd_path+rpd_extension)
-			delete_file(original_rpd_path+rpd_extension)
-			delete_file(command_file_name)
-			delete_file(decisions_temp_file)
+			try:
+				delete_file(current_rpd_path+rpd_extension)
+				delete_file(modified_rpd_path+rpd_extension)
+				delete_file(original_rpd_path+rpd_extension)
+				delete_file(command_file_name)
+				delete_file(decisions_temp_file)
+			except Exception as e:
+				throw new Exception("Error when releasing resources: "+str(e))
 		elif action == "diff":
 			# diff actions
 			# validate inputs
 			if len(sys.argv) <= 7:
+				log(logging.ERROR, "Not enough arguments for diff")
 				raise Exception("Not enough arguments for diff.");	
+			log(logging.DEBUG, "Starting diff workflow")
 			my_file = sys.argv[2]
 			first_file = sys.argv[3]
 			second_file = sys.argv[6]
@@ -195,14 +228,19 @@ if __name__ == "__main__":
 					output_string+="\n"+item_change_icon + " ("+item_change+") "+item_type+ " with name "+item_name+" in "+item_location+" layer."
 			
 			# clean up
-			delete_file(command_file_name)
-			delete_file(output_file_path)
+			try:
+				delete_file(command_file_name)
+				delete_file(output_file_path)
+			except Exception as e:
+				throw new Exception("Error releasing resources: "+str(e))
 			print(output_string)
 		else:
+			log(logging.ERROR, "No valid action: "+action)
 			raise Exception("No valid action found: '"+action+"'. Valid actions: (merge, diff)");
 		
 		# exit with success code
 		exit(0)
 	except Exception as e:
+		log(logging.ERROR, "Unexpected error occurred: \n"+str(e))
 		print("Error - "+str(e))
 		exit(1)
